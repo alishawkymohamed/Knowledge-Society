3178d236de104f26e2618c4dfa3599fcf0a841ec
ali.shawky.mohamed <pet.eng.ali.shawky@gmail.com>
2017-11-25 16:06:09 +0200

    ALi

--- src/app/signup/birth-data/birth-data.component.ts
+++ src/app/signup/birth-data/birth-data.component.ts
@@ -34,47 +34,47 @@ export class BirthDataComponent implements OnInit {
 
   User: any = {};
 
-  constructor(private ApiService: ApiService,private BirthData : BirthDataService,private UserDataService: UserDataService,private router: Router,private RegisterService: RegisterService,public toastr: ToastsManager, vcr: ViewContainerRef) {
-    if(UserDataService.getUserData() == null){
-     router.navigateByUrl('/account/signup/personaldata');
+  constructor(private ApiService: ApiService, private BirthData: BirthDataService, private UserDataService: UserDataService, private router: Router, private RegisterService: RegisterService, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    if (UserDataService.getUserData() == null) {
+      router.navigateByUrl('/account/signup/personaldata');
     }
-    else{
+    else {
       this.User = UserDataService.getUserData();
-      this.BirthGovernment=this.User.POBGovernateID ;
+      this.BirthGovernment = this.User.POBGovernateID;
       this.BirthArea = this.User.POBAreaID;
-      this.BirthVillage=this.User.POBVillageID;
+      this.BirthVillage = this.User.POBVillageID;
       this.livingGovernment = this.User.AddressGovernateID;
       this.livingArea = this.User.AddressAreaID;
       this.livingVillage = this.User.AddressVillageID;
     }
-    if(BirthData.GetData().Governrates.length == 0){
-    ApiService.ServerRequest('/GeneralData/GetBirthData','GET',null).subscribe(
-      (data) => {
-        BirthData.SetData(data);
-        const Sdata = BirthData.GetData();
-        this.GovernrateArray = Sdata.Governrates;
-        this.AreaArray = Sdata.Areas;
-        this.VillageArray = Sdata.Villages;
-        this.loading = false;
-      }
-    )
-  }
-  else{
-    this.loading = false
+    if (BirthData.GetData().Governrates.length == 0) {
+      ApiService.ServerRequest('/GeneralData/GetBirthData', 'GET', null).subscribe(
+        (data) => {
+          BirthData.SetData(data);
+          const Sdata = BirthData.GetData();
+          this.GovernrateArray = Sdata.Governrates;
+          this.AreaArray = Sdata.Areas;
+          this.VillageArray = Sdata.Villages;
+          this.loading = false;
+        }
+      )
     }
-   }
+    else {
+      this.loading = false
+    }
+  }
 
   ngOnInit() {
 
   }
-  SelectBirthGoverment(ID: any){
+  SelectBirthGoverment(ID: any) {
     this.SelectedArias = this.AreaArray.filter(
       (value) => {
         return Number.parseInt(value.GovernateID) === Number.parseInt(ID);
       }
     );
   }
-  SelectLivingGoverment(ID: any){
+  SelectLivingGoverment(ID: any) {
     this.LivingAreaArray = this.AreaArray.filter(
       (value) => {
         return Number.parseInt(value.GovernateID) === Number.parseInt(ID);
@@ -82,7 +82,7 @@ export class BirthDataComponent implements OnInit {
     );
   }
 
-  SelectBirthVillage(ID:any){
+  SelectBirthVillage(ID: any) {
     this.VillageArray = this.AreaArray.filter(
       (value) => {
         return Number.parseInt(value.AreaID) === Number.parseInt(ID);
@@ -90,20 +90,20 @@ export class BirthDataComponent implements OnInit {
     );
   }
 
-  SelectLivingVillage(ID:any){
+  SelectLivingVillage(ID: any) {
     this.VillageArray = this.AreaArray.filter(
       (value) => {
         return Number.parseInt(value.AreaID) === Number.parseInt(ID);
       }
     );
   }
-  OnSubmit(){
-    this.User.POBGovernateID = this.BirthGovernment==0?null:this.BirthGovernment;
-    this.User.POBAreaID = this.BirthArea==0?null:this.BirthArea;
-    this.User.POBVillageID = this.BirthVillage==0?null:this.BirthVillage;
-    this.User.AddressGovernateID = this.livingGovernment==0?null:this.livingGovernment;
-    this.User.AddressAreaID = this.livingArea==0?null:this.livingArea;
-    this.User.AddressVillageID = this.livingVillage==0?null:this.livingVillage;
+  OnSubmit() {
+    this.User.POBGovernateID = this.BirthGovernment == 0 ? null : this.BirthGovernment;
+    this.User.POBAreaID = this.BirthArea == 0 ? null : this.BirthArea;
+    this.User.POBVillageID = this.BirthVillage == 0 ? null : this.BirthVillage;
+    this.User.AddressGovernateID = this.livingGovernment == 0 ? null : this.livingGovernment;
+    this.User.AddressAreaID = this.livingArea == 0 ? null : this.livingArea;
+    this.User.AddressVillageID = this.livingVillage == 0 ? null : this.livingVillage;
     this.RegisterService.SendRegisterData({ TabName: "Birth Data", PersonalData: this.User }).subscribe(
       (Response) => {
         if (Response != false) {
@@ -121,5 +121,5 @@ export class BirthDataComponent implements OnInit {
   }
 
 
-  }
+}
 
--- src/app/signup/work-data/work-data.component.ts
+++ src/app/signup/work-data/work-data.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, ViewEncapsulation, ViewContainerRef } from '@angular/core';
+import { Component, ViewEncapsulation, ViewContainerRef, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from '../../shared/api.service';
 import { BirthDataService } from '../../shared/birth-data.service';
 import { UserDataService } from '../../shared/user-data.service';
 import { ToastsManager } from 'ng2-toastr';
+import { WorkDataService } from '../../shared/work-data.service';
+import { RegisterService } from '../../shared/register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-work-data',
@@ -11,43 +14,122 @@ import { ToastsManager } from 'ng2-toastr';
   encapsulation: ViewEncapsulation.None
 })
 export class WorkDataComponent {
+
   public loading = false;
 
+  User: any = {};
   GovernrateArray = [];
   AreaArray = [];
   VillageArray = [];
+  WorkTypes = [];
+  WorkEquaties = [];
+  WorkRelation = [];
+  WorkDuration = [];
+  WorkTimeByYear = [];
+  WorkStatus = [];
   SelectedArias = [];
   selectedVillages = [];
 
-  WorkGovernment: any = 0;
-  WorkArea: any = 0;
-  WorkVillage: any = 0;
+  WorkGovernmentID: any = 0;
+  WorkAreaID: any = 0;
+  WorkVillageID: any = 0;
+  WorkEquityID: any = 0;
+  WordDurationID: any = 0;
+  WorkRelationID: any = 0;
+  WorkTypeID: any = 0;
+  WorkTimePerYearID: any = 0;
+  WorkStatusID: any = 0;
+  IsCurrent: boolean = null;
 
   constructor(private ApiService: ApiService,
     private BirthData: BirthDataService,
-    UserDataService: UserDataService,
-    public toastr: ToastsManager, vcr: ViewContainerRef) {
+    private UserDataService: UserDataService,
+    public toastr: ToastsManager,
+    vcr: ViewContainerRef,
+    private router: Router,
+    private RegisterService: RegisterService,
+    private WorkDataService: WorkDataService) {
 
+    this.toastr.setRootViewContainerRef(vcr); // Toaster
     this.loading = true;
-    if (BirthData.GetData().Governrates.length == 0) {
-      ApiService.ServerRequest('/GeneralData/GetBirthData', 'GET', null).subscribe(
-        (data) => {
-          if (data) {
-            this.loading = false;
+
+    if (this.UserDataService.getUserData() == null) {
+      this.toastr.info("من فضلك أدخل البيانات الأساسية أولا !!", "تنبيه");
+      setTimeout(() => {
+        this.router.navigate(['/account', 'signup', 'personaldata']);
+      }, 3000);
+    }
+    else {
+      if (this.WorkDataService.GetData() == null) {
+        this.ApiService.ServerRequest('/GeneralData/GetWorkData', 'GET', null).subscribe(
+          (data) => {
+            this.WorkDataService.SetData(data);
+            const Sdata = this.WorkDataService.GetData();
+
+            this.GovernrateArray = Sdata.WorkGoverates;
+            this.VillageArray = Sdata.WorkVillage;
+            this.AreaArray = Sdata.WorkArea;
+            this.WorkTypes = Sdata.WorkTypes;
+            this.WorkDuration = Sdata.WorkDuration;
+            this.WorkRelation = Sdata.WorkRelation;
+            this.WorkTimeByYear = Sdata.WorkTimeByYear;
+            this.WorkEquaties = Sdata.WorkEquaties;
+            this.WorkStatus = Sdata.WorkStatus;
+
+            this.LoadUserData();
+
+            if (data) {
+              this.loading = false;
+            }
+          },
+          (error) => {
+            if (error) {
+              this.loading = false;
+              this.toastr.error("لقد حدث خطأ ما .. من فضلك أعد المحاولة لاحقاً !!", 'خطأ!');
+            }
           }
-          BirthData.SetData(data);
-          const Sdata = BirthData.GetData();
-          this.GovernrateArray = Sdata.Governrates;
-          this.AreaArray = Sdata.Areas;
-          this.VillageArray = Sdata.Villages;
-        },
-        (error) => {
-          if (error) {
-            this.loading = false;
-            this.toastr.error("لقد حدث خطأ ما .. من فضلك أعد المحاولة لاحقاً !!", 'خطأ!');
-          }
-        }
-      )
+        )
+      }
+      else {
+        const Sdata = this.WorkDataService.GetData();
+
+        this.GovernrateArray = Sdata.WorkGoverates;
+        this.VillageArray = Sdata.WorkVillage;
+        this.AreaArray = Sdata.WorkArea;
+        this.WorkTypes = Sdata.WorkTypes;
+        this.WorkDuration = Sdata.WorkDuration;
+        this.WorkRelation = Sdata.WorkRelation;
+        this.WorkTimeByYear = Sdata.WorkTimeByYear;
+        this.WorkEquaties = Sdata.WorkEquaties;
+        this.WorkStatus = Sdata.WorkStatus;
+
+        this.LoadUserData();
+        this.loading = false;
+      }
+    }
+
+  }
+
+  LoadUserData() {
+    if (this.UserDataService.getUserData() != null) {
+      debugger;
+      this.User = this.UserDataService.getUserData();
+      this.WorkGovernmentID = this.User.PersonWorkData.GovernateID == null ? 0 : this.User.PersonWorkData.GovernateID;
+      if (this.WorkGovernmentID != null) {
+        this.SelectWorkGoverment(Number.parseInt(this.WorkGovernmentID));
+      }
+      this.WorkAreaID = this.User.PersonWorkData.AreaID == null ? 0 : this.User.PersonWorkData.AreaID;
+      if (this.WorkAreaID != null) {
+        this.SelectWorkArea(Number.parseInt(this.WorkAreaID));
+      }
+      this.IsCurrent = this.User.PersonWorkData.IsCurrent;
+      this.WordDurationID = this.User.PersonWorkData.JobDurationID;
+      this.WorkTimePerYearID = this.User.PersonWorkData.JobHoursPerYearID;
+      this.WorkRelationID = this.User.PersonWorkData.JobRelationID;
+      this.WorkVillageID = this.User.PersonWorkData.VillageID == null ? 0 : this.User.PersonWorkData.VillageID;
+      this.WorkEquityID = this.User.PersonWorkData.WorkEquityID;
+      this.WorkStatusID = this.User.PersonWorkData.WorkStatusID;
+      this.WorkTypeID = this.User.PersonWorkData.WorkTypeID;
     }
   }
 
@@ -57,13 +139,86 @@ export class WorkDataComponent {
         return Number.parseInt(value.GovernateID) === Number.parseInt(ID);
       }
     );
+    this.WorkAreaID = 0;
+    this.WorkGovernmentID = ID;
   }
 
-  SelectWorkVillage(ID: any) {
+  SelectWorkArea(ID: any) {
     this.VillageArray = this.AreaArray.filter(
       (value) => {
         return Number.parseInt(value.AreaID) === Number.parseInt(ID);
       }
     );
+    this.WorkVillageID = 0;
+    this.WorkAreaID = ID;
+  }
+  SelectWorkVillage(ID: any) {
+    this.WorkVillageID = ID;
+  }
+  SelectWorkRelation(ID: any) {
+    this.WorkRelationID = ID;
+  }
+
+  SelectWorkType(ID: any) {
+    this.WorkTypeID = ID;
+  }
+
+  SelectWorkEquity(ID: any) {
+    this.WorkEquityID = ID;
+  }
+
+  SelectWorkDuration(ID: any) {
+    this.WordDurationID = ID;
+  }
+
+  SelectWorkTimePerYear(ID: any) {
+    this.WorkTimePerYearID = ID;
+  }
+
+  SelectWorkStatus(ID: any) {
+    this.WorkStatusID = ID;
+  }
+
+  SetIsCurrent(IsCurrent) {
+    this.IsCurrent = (IsCurrent == 0) ? false : true;
+  }
+
+  SendWorkData() {
+    this.loading = true;
+    let personalData = {
+      TabName: "Work Data",
+      PersonalData: {
+        Id: this.User.ID,
+        PersonWorkData: {
+          PersonID: this.User.ID,
+          GovernateID: this.WorkGovernmentID,
+          AreaID: this.WorkAreaID,
+          VillageID: this.WorkVillageID,
+          WorkEquityID: this.WorkEquityID,
+          WorkTypeID: this.WorkTypeID,
+          JobRelationID: this.WorkRelationID,
+          IsCurrent: this.IsCurrent,
+          JobDurationID: this.WordDurationID,
+          JobHoursPerYearID: this.WorkTimePerYearID,
+          WorkStatusID: this.WorkStatusID
+        }
+      }
+    }
+    this.RegisterService.SendRegisterData(personalData).subscribe(
+      (data) => {
+        if (data != false) {
+          this.UserDataService.setUserData(data);
+          this.loading = false;
+          this.toastr.success("تم تسجيل البيانات بنجاح ..");
+          setTimeout(() => {
+            this.router.navigate(['/account', 'signup', 'educationdata']);
+          }, 3000);
+        }
+        else {
+          this.loading = false;
+          this.toastr.error("لقد حدث خطأ ما .. من فضلك أعد المحاولة لاحقاً !!", 'خطأ!');
+        }
+      }
+    )
   }
 }
